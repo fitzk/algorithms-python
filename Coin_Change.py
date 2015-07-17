220d943e12ac6ee2700b288ad247ec9449dcfe6c
vagrant <vagrant@precise32.(none)>
2015-07-17 21:08:13 +0000

    start brute force alg, add file to test brute force alg

--- Coin_Change.py
+++ Coin_Change.py
@@ -3,15 +3,48 @@
 # Group Members:  Eric Cruz, Baljot Singh, Kayla Fitzsimmons         #
 # Course:         CS325-400                                          #
 # Assignment:     Project 2                                          #
-# File:           Coin_Change.py                                          #
+# File:           Coin_Change.py                                     #
 # Description:    Object that contains three algorithms that solve   #
 #                 the coin change problem                            #
 ######################################################################
 class Coin_Change(object):
-	
+
+	#brute force divide and conquer algorithm
+	def changeslow(self,coins, amount):
+		#if there is a K-cent coin then that one coin is the minimum
+
+
+		# j=0
+		# while j < len(coins):
+		# 	if coins[j] == amount:
+		# 		print coins[j]
+		# 		return coins[j]
+		# 	j=j+1
+		i=1
+
+		while i <= amount:
+			print i
+			print amount
+		 	coins_a = self.changeslow(coins, i)
+			coins_b = self.changeslow(coins, amount - i)
+			print "Coints at a: "
+			print coins_a
+			i = i + 1
+		if amount == 1:
+			return 1
+		#
+		# 	new_coins = coins_a + coins_b
+		# 	print new_coins
+		# 	i = i +1
+		# return "fin"
+
+
+
+
+
 	#Greedy algorithm
 	def changegreedy(self, V, A):
-    		coinUsed = []  # type of coins used 
+    		coinUsed = []  # type of coins used
     		m = 0  # minimum number of coins used
 
     		# reverses V to be in decreasing order
@@ -21,17 +54,17 @@ class Coin_Change(object):
         			A = A - V[length]
         			m = m + 1
             			coinUsed.append(V[length])
-        		
+
         		elif A < V[length]:
             			length = length - 1
-    
+
         	return m, coinUsed
-    		
+
     	#coin denomination using dynammic programming
 	def coinDen(self, coins, amount):
 		table = []    #table to keep tarck of number of coins
     		coinUsed = []  #table to keep tarck of coins used
-    
+
     		for i in range(0, amount + 1):
         		table.append(i)
         		coinUsed.append(1)
@@ -41,5 +74,5 @@ class Coin_Change(object):
             			if table[j] > table[j - i] + 1:
                 			table[j] = table[j - i] + 1
                 			coinUsed[j] = i
-                
+
     		return table[amount], coinUsed
---
+++ test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+from Coin_Change import Coin_Change
+
+
+coin = Coin_Change()
+
+coins = [1,3,5,10]
+coin.changeslow(coins, 4)
